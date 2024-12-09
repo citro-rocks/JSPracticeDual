3d509f02ca90df189e603529497b6bc74a6e5792
Nikola Naskov <citro.rocks.web@gmail.com>
2024-12-09 12:20:35 +0100

    init

--- Practice_One/script.js
+++ Practice_One/script.js
@@ -8,12 +8,20 @@ inputField.onkeydown = (event) => {
     }
 };
 
+
 const buttonActive = document.getElementById('drawButton');
 document.getElementById('drawButton').addEventListener('click',
     function() {
         const number = parseInt(document.getElementById('input-value').value);
         const shadeBox = document.getElementById('shade-box');
         shadeBox.innerHTML = '';
+
+        const numberLimit = document.getElementById('input-value');
+        numberLimit.addEventListener('change', function() {
+            let limit = perseInt(this.value);
+            if (limit < 1)this.value = 1;
+            if (limit > 50)this.value = 50;
+        })
     
         for (let i = 0; i < number; i++) {
             const element = document.createElement('div');
@@ -24,14 +32,15 @@ document.getElementById('drawButton').addEventListener('click',
             element.textContent = `${i + 1}. nijansa sive`;
             element.style.color = textColor;
             shadeBox.appendChild(element);
-        } 
 
-        if(number > 50) {
-            alert('Unesite broj manji od 51');
-            // const alert = document.inputSEction.appendChild('label');
-            // alert.inerText = 'Unesite broj manji od 51';
-        }
+            
+            if(number > 50) {
+                shadeBox.textContent = `Unesite broj manji od 51`;
+            }
+        } 
     }
+
+    
 
 );
 
